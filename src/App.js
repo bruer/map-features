84d0fcb3e00a42ee06cda49692d03a21796fb0d0
bruer <f.bruer@gmail.com>
2021-03-09 12:41:39 +0100

    setup leaflet map

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { MapContainer, TileLayer } from 'react-leaflet'
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <MapContainer center={[59.34573368442725, 17.998488864382967]} zoom={15} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </div>
   );
 }
 
